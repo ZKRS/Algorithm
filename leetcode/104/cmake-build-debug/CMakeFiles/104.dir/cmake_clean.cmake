a6a31ec35911686210f6111dc895ce9678d48e93
ZKRS <2585122837@qq.com>
2018-03-16 22:55:13 +0800

    20180316

---
+++ leetcode/104/cmake-build-debug/CMakeFiles/104.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/104.dir/main.cpp.o"
+  "104.pdb"
+  "104.exe"
+  "lib104.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/104.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
