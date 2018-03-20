23ec0449b1ec490f2931e007ab38f84f3e9a1f0e
ZKRS <2585122837@qq.com>
2018-03-20 09:31:09 +0800

    2018-03-18

---
+++ leetcode/235/cmake-build-debug/CMakeFiles/235.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/235.dir/main.cpp.o"
+  "235.pdb"
+  "235.exe"
+  "lib235.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/235.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ leetcode/236/cmake-build-debug/CMakeFiles/236.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/236.dir/main.cpp.o"
+  "236.pdb"
+  "236.exe"
+  "lib236.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/236.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
