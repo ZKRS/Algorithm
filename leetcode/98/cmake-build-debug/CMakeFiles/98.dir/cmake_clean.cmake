d7591979985d6fff36052aa549f56baba84f2acf
ZKRS <2585122837@qq.com>
2018-03-20 22:44:45 +0800

    2018-03-20

---
+++ leetcode/98/cmake-build-debug/CMakeFiles/98.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/98.dir/main.cpp.o"
+  "98.pdb"
+  "98.exe"
+  "lib98.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/98.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
