f7c9f84f7f5d8db79febefefb3712c76f9f01f20
ZKRS <2585122837@qq.com>
2018-03-21 23:58:35 +0800

    2018-03-21

--- leetcode/98/main.cpp
+++ leetcode/98/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <climits>
+#include <vector>
+#include <stack>
+
+using namespace std;
 
 /**
  * Definition for a binary tree node.
@@ -22,13 +26,12 @@ struct TreeNode {
 
 class Solution {
 public:
-    TreeNode *pre;
-    = nullptr;
+    TreeNode *pre = nullptr;
 
     bool isValidBST(TreeNode *root) {
         if (root == nullptr) return true;
         return solve(root);
-        return solve2(root, LONG_MIN, LONG_MAX);
+//        return solve2(root, LONG_MIN, LONG_MAX);
     }
 
     bool solve(TreeNode *root) {
@@ -36,7 +39,7 @@ public:
 
         if (!solve(root->left)) return false;
 
-        if (pre != nullptr && pre->val > root->val) return false;
+        if (pre != nullptr && pre->val >= root->val) return false;
 
         pre = root;
 
@@ -50,6 +53,28 @@ public:
 
         return solve2(root->left, min, root->val) && solve2(root->right, root->val, max);
     }
+
+    /**
+     * 二叉树的非递归中序遍历
+     */
+    vector<int> innerOrder(TreeNode *root) {
+        vector<int> res;
+        if (root == nullptr) return res;
+        stack<TreeNode *> s;
+        while (root != nullptr || !s.empty()) {
+
+            while (root != nullptr) {
+                s.push(root);
+                root = root->left;
+            }
+            root = s.top();
+            res.push_back(root->val);
+            s.pop();
+            root = root->right;
+        }
+        return res;
+    }
+
 };
 
 int main() {
