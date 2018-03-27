599a0505434d2c7beac028eb5301b51efb65bb37
ZKRS <2585122837@qq.com>
2018-03-27 21:55:46 +0800

    2018-03-27

--- datastructure/graph/ConnectedComponents.h
+++ datastructure/graph/ConnectedComponents.h
@@ -5,11 +5,16 @@
 #ifndef GRAPH_CONNECTEDCOMPONENTS_H
 #define GRAPH_CONNECTEDCOMPONENTS_H
 /**
- * 无权图
- * 第一行输入顶点数n,边数m
- * 接下来m行输入顶点对之间的关系
- * 输入查询问题数q
- * 接下来q行输入顶点对, 以查询顶点对之间是否存在关系
+ * 无权图中查找连通分量:
+     * 深度优先搜索应用实例
+     * 广度优先搜索应用实例
+ *
+ *
+ * 要求:
+     * 第一行输入顶点数n,边数m
+     * 接下来m行输入顶点对之间的关系
+     * 输入查询问题数q
+     * 接下来q行输入顶点对, 以查询顶点对之间是否存在关系
  */
 
 #endif //GRAPH_CONNECTEDCOMPONENTS_H
---
+++ datastructure/graph/DepthFirstSearch.h
@@ -0,0 +1,20 @@
+//
+// Created by wangqing on 2018.3.27.
+//
+
+#ifndef GRAPH_DEPTHFIRSTSEARCH_H
+#define GRAPH_DEPTHFIRSTSEARCH_H
+
+#include <iostream>
+#include <vector>
+#include <stack>
+
+#define UNVISITED 0
+#define ALREADY_IN_STACK 1
+#define ALREADY_POP_FROM_STACK 2
+
+using namespace std;
+
+vector<int> node_status; // 记录结点访问状态
+
+#endif //GRAPH_DEPTHFIRSTSEARCH_H
