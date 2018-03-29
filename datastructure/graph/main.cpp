58cd08a3404fd2528ab09edb5a83fa46b0c244ff
ZKRS <2585122837@qq.com>
2018-03-29 22:09:42 +0800

    2018-03-29

--- datastructure/graph/main.cpp
+++ datastructure/graph/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include "SparseGraph.h"
-#include "DenseGraph.h"
-#include "ReadGraph.h"
+#include "SingleSourceShortestPath.h"
+//#include "SparseGraph.h"
+//#include "DenseGraph.h"
+//#include "ReadGraph.h"
 
 using namespace std;
 
@@ -12,18 +13,20 @@ using namespace std;
  * @return
  */
 int main() {
-    string filename = "testG1.txt";
-    SparseGraph g(13, false);
-    ReadGraph<SparseGraph> readGraph1(g, filename);
-    cout << "test G1 in Sparse Graph:" << endl;
-    g.show();
+//    string filename = "testG1.txt";
+//    SparseGraph g(13, false);
+//    ReadGraph<SparseGraph> readGraph1(g, filename);
+//    cout << "test G1 in Sparse Graph:" << endl;
+//    g.show();
+//
+//    DenseGraph g2(13, false);
+//    ReadGraph<DenseGraph> readGraph2(g2, filename);
+//    cout << "test G1 in Dense Graph:" << endl;
+//    g2.show();
+//
+//
+//    cout << endl;
 
-    DenseGraph g2(13, false);
-    ReadGraph<DenseGraph> readGraph2(g2, filename);
-    cout << "test G1 in Dense Graph:" << endl;
-    g2.show();
-
-
-    cout << endl;
+    dijkstra();
     return 0;
 }
